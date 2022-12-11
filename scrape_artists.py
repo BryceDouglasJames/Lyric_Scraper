48117641ec2a576254a7b93f1a0753060f7e8197
BryceDouglasJames <brycejames1214@yahoo.com>
2022-12-11 04:24:23 -0500

    Almost done with complete artist album/song scrape

--- scrape_artists.py
+++ scrape_artists.py
@@ -1,7 +1,22 @@
-
+from dotenv import load_dotenv
+from lyricsgenius import Genius
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import os
+
+#Get environment variables
+load_dotenv()
+
+#Init lyrics genius API worker
+genius = Genius(os.getenv("GENIUS_KEY"))
+
+#Set API Settings
+genius.timeout = 60
+genius.verbose = True
+genius.remove_section_headers = True
+genius.skip_non_songs = True
+genius.excluded_terms = ["(Remix)", "(Live)"]
 
 # Open up safari browser instance
 driver = webdriver.Safari()
@@ -9,7 +24,7 @@ driver.get("https://genius.com/verified-artists")
 
 # Scroll alllll the way down to get a full set of artists
 load_counter = 0
-while(load_counter < 100):
+while(load_counter < 1):
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") 
     time.sleep(1)
     load_counter+=1
@@ -19,9 +34,13 @@ links = driver.find_elements(By.CLASS_NAME, "login")
 
 # Write artists names into a txt file and close it
 f = open("artists.txt", "a")
+
+# Go through each artist and get tuple their name and id to a txt file
 for artist in links:
-    f.write(artist.text + '\n')
+    id = genius.search(artist.text)['hits'][0]["result"]['primary_artist']['id']
+    f.write(artist.text + '\t' + str(id) + '\n')
 f.close()
+
 
 #This only grabs the artists upong the forst request. We need something that can dynamically load more artists...
 '''
--- scraper.py
+++ scraper.py
@@ -9,14 +9,85 @@ load_dotenv()
 genius = Genius(os.getenv("GENIUS_KEY"))
 
 #Set API Settings
+genius.timeout = 60
 genius.verbose = True
 genius.remove_section_headers = True
 genius.skip_non_songs = True
-genius.excluded_terms = ["(Remix)", "(Live)"]
+genius.excluded_terms = ["(Remix)", "(Live)", "Single"]
+names = []
+try:
+    with open("artists.txt") as artists:
+       for line in artists:
+        names.append(str(line))
+
+    for artist in names:
+        # Path 
+        formatted = artist.split('\t')
+        name = formatted[0]
+        id = formatted[1]
+
+        path = os.path.join("./", name) 
+        if not os.path.exists(path):
+           os.mkdir(path)
+        os.chdir(path)      
+
+        all_lyrics_from_artist = ""
+        all_lyrics_from_album = ""
+        all_lyrics_from_song = ""
+        all_albums = {}
+
+
+        albums_search = genius.artist_albums(id)['albums']
+        for key in albums_search:
+            #TODO to be safe, we should instead grab the id for the album instead of the title...
+            album = str(key['name'])
+            print(album)
+            if not os.path.exists(album):
+                os.mkdir(album)
+            os.chdir(album)
+
+            songs = genius.search_album(key['name'])
+           
+            #Keep for debug
+            #for t in songs.to_dict()['tracks'][0]['song']:
+            #    print(t)   
+            #print(songs.to_dict()['tracks']['song']['lyrics'])
+
+            album_dict = songs.to_dict()['tracks']
+            for songs_info in album_dict:
+                index = int(songs_info['number']) - 1
+                
+                temp_map = album_dict[index]['song']
+                song_name = temp_map['title']
+                song_lyrics = temp_map['lyrics']
+
+                if "/" in song_name: 
+                    temp = song_name.split("/")
+                    new_name = ""
+                    for s in temp:
+                        new_name = s + "\\"
+                    song_file = open(new_name + ".txt", "a")
+                    song_file.write(song_lyrics)
+                else:
+                    song_file = open(song_name + ".txt", "a")
+                    song_file.write(song_lyrics)
+                song_file.close()
+
+            all_lyrics_from_album = open("_albumall.txt" , "a")
+            all_lyrics_from_album.write(songs.to_text())
+            all_lyrics_from_album.close()
+
+            os.chdir("..")
+            #TODO write txt file for all the artists lyrics
+    
+except:
+    raise RuntimeError("There was a problem when reading from artists file!")
+
+
 
 #Search song
-song = genius.search_song("The Motto", "Drake")
-lyr = song.lyrics
+#song = genius.search_song("The Motto", "Drake")
+#lyr = song.lyrics
 
 #Print lyrics of song
-print(lyr)
+#print(lyr)
